016ecd20ff468aa7775f0390f2613504c0a908c7
Paweł Gancarczyk <beznadziejnatapoczta@gmail.com>
2019-12-11 14:18:07 +0100

    sample API request, waiting for Mateusz Natanek to update CORS header on the server side...

--- src/components/TvSeries.vue
+++ src/components/TvSeries.vue
@@ -1,13 +1,35 @@
 <template>
     <div>
-        <p>tvserieslist</p>
+        <p>tvseries</p>
         <p>id = {{ $route.params.id }}</p>
+        <div>
+            <code>
+                {{ response }}
+            </code>
+        </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
-        name: "TvSeriesList"
+        name: "TvSeriesList",
+        data() {
+            return {
+                response: 'loading...'
+            }
+        },
+        mounted() {
+            axios
+                .get('https://tv-series-ssi.herokuapp.com/api/tvseries/1', { headers: { 'Access-Control-Allow-Origin': '*' } })
+                .then(response => {
+                    this.response = JSON.stringify(response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
     }
 </script>
 
